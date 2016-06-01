ed2e43a329e4ace58c835f145d9275b6c19103ea
jbkopecky <jb.kopecky@gmail.com>
2016-06-01 12:08:16 +0200

    wallpaper

--- scripts/wall.sh
+++ scripts/wall.sh
@@ -1,5 +1,5 @@
 DEFAULT_WALL="$HOME/Pictures/Wallpapers/sea_sunrise.jpg"
-WALL="/home/jb/Pictures/Wallpapers/mist_mountain.jpg"
+WALL="/home/jb/Pictures/Wallpapers/sea_sunrise.jpg"
 
 if [[ -f $WALL ]]; then
     feh --bg-fill "$WALL"
